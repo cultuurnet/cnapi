59940dd364afee3f7c039f28ea0ed9cf8066fa84
zuuperman <nils.destoop@gmail.com>
2012-11-06 15:28:17 +0100

    Facets on overview page

--- cnapi_ui/cnapi-ui-event-mini-summary.tpl.php
+++ cnapi_ui/cnapi-ui-event-mini-summary.tpl.php
@@ -1,34 +1,35 @@
-<div id="cnapi-object-<?php print $cdbid; ?>" class="<?php print $classes; ?> mini-teaser clearfix"<?php print $attributes; ?>>
+<div class="image">
 
-  <div class="teaser-image">
-  
-    <?php if ($thumbnail) : ?>
-      <?php print $thumbnail ?>
-    <?php endif; ?>
-
-  </div>
-  
-  <div class="teaser-text">
-
-    <?php print render($title_prefix); ?>
-    <h<?php print $heading_level ?><?php print $title_attributes; ?>>
-      <a href="<?php print $object_url; ?>"><?php print $title; ?></a>
-    </h<?php print $heading_level ?>>
-    <?php print render($title_suffix); ?>
-  
-    <?php if ($where) : ?>
-      <div class="where"><?php print $where ?></div>
-    <?php endif; ?>
-  
-    <?php if ($when) : ?>
-      <div class="when"><?php print $when ?></div>
-    <?php endif; ?>
-    <div class="actions">
-      <?php if ($like_link) : ?><div class="like"><?php print $like_link ?></div><?php endif; ?><?php if ($remove_link) : ?><div class="remove"><?php print $remove_link ?></div><?php endif; ?>
-    </div>
-
-    <div class="clearfix"></div>
-    
-  </div>
+  <?php if ($thumbnail) : ?>
+    <?php print $thumbnail ?>
+  <?php endif; ?>
 
 </div>
+
+<?php print render($title_prefix); ?>
+<h<?php print $heading_level ?><?php print $title_attributes; ?>>
+  <a href="<?php print $object_url; ?>"><?php print $title; ?></a>
+</h<?php print $heading_level ?>>
+<?php print render($title_suffix); ?>
+
+<?php if ($when) : ?>
+  <div class="when"><?php print $when ?></div>
+<?php endif; ?>
+
+<?php if ($where) : ?>
+  <div class="where">
+    <span class="where-location"><?php print $location ?></span>
+    <span class="where-city"><?php print $city ?></span>
+  </div>
+<?php endif; ?>
+
+<div class="more">
+  <?php print $more_link ?>
+</div>
+
+<div class="actions">
+  <?php if ($like_link) : ?><div class="like"><?php print $like_link ?></div><?php endif; ?>
+  <?php if ($remove_link) : ?><div class="remove"><?php print $remove_link ?></div><?php endif; ?>
+</div>
+
+<div class="clearfix"></div>
