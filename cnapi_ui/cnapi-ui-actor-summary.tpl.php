4fa4ecda5dabece191214f828a43ed5feb62e40c
Luk Dens <luk.dens@cultuurnet.be>
2014-04-11 17:19:19 +0200

    UIV-750 theme actor summary

--- cnapi_ui/cnapi-ui-actor-summary.tpl.php
+++ cnapi_ui/cnapi-ui-actor-summary.tpl.php
@@ -1,32 +1,42 @@
-<div id="cnapi-object-<?php print $cdbid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<div id="cnapi-object-<?php print $cdbid; ?>" class="<?php print $classes; ?> teaser clearfix"<?php print $attributes; ?>>
 
-  <?php print render($title_prefix); ?>
-  <h<?php print $heading_level ?><?php print $title_attributes; ?>>
-    <a href="<?php print $object_url; ?>"><?php print $title; ?></a>
-  </h<?php print $heading_level ?>>
-  <?php print render($title_suffix); ?>
+  <div class="teaser-image">
+  
+    <?php if ($thumbnail) : ?>
+      <?php print $thumbnail ?>
+    <?php endif; ?>
 
-  <?php if ($thumbnail) : ?>
-    <?php print $thumbnail ?>
-  <?php endif; ?>
+  </div>
 
-  <?php if ($shortdescription) : ?>
-    <div class="description">
-      <?php print $shortdescription ?>
+  <div class="teaser-text">
+
+    <?php print render($title_prefix); ?>
+    <h<?php print $heading_level ?><?php print $title_attributes; ?>>
+      <a href="<?php print $object_url; ?>"><?php print $title; ?></a>
+    </h<?php print $heading_level ?>>
+    <?php print render($title_suffix); ?>
+
+    <?php if ($shortdescription) : ?>
+      <div class="description">
+        <?php print $shortdescription ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($where) : ?>
+      <dl class="clearfix">
+
+        <?php if ($where) : ?>
+          <dt>Waar</dt>
+          <dd><?php print $where ?></dd>
+        <?php endif; ?>
+
+      </dl>
+    <?php endif; ?>
+
+    <div class="teaser-actions">
+      <?php print $more_link ?>
     </div>
-  <?php endif; ?>
-  
-  <?php if ($where) : ?>
-    <dl class="clearfix">
-  
-      <?php if ($where) : ?>
-        <dt>Waar</dt>
-        <dd><?php print $where ?></dd>
-      <?php endif; ?>
-  
-    </dl>
-  <?php endif; ?>
 
-  <?php print $more_link ?>
+  </div>
 
 </div>
